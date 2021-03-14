4e4961ed029ceff7e56a7a392e02c1c5cbaabb56
TotallyNotElite <totallynotelite@gmail.com>
2021-03-14 13:05:56 +0100

    gitlab ci

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+mkdir -p tools && pushd tools
+wget -N https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+wget -N https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
+wget -N https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/releases/download/continuous/linuxdeploy-plugin-appimage-x86_64.AppImage
+chmod +x *.AppImage
+popd
+
+rm -r build | true
+mkdir -p build && cp ./resources/Garuda-Downloader.desktop ./build/ && cp ./resources/garuda.svg ./build/garuda-downloader.svg && cd build
+cmake -DCMAKE_BUILD_TYPE=Release .. && make
+../tools/linuxdeploy-x86_64.AppImage --plugin qt --executable=./garuda-downloader --appdir=./appdir/ --desktop-file ./Garuda-Downloader.desktop --icon-file ./garuda-downloader.svg
+rm -r ./appdir/usr/translations | true
+rm -r ./appdir/usr/doc | true
+rm -r ./squashfs-root | true
+\cp --remove-destination ./appdir/usr/share/applications/Garuda-Downloader.desktop ./appdir
+../tools/linuxdeploy-plugin-appimage-x86_64.AppImage --appdir ./appdir
--- garuda-downloader_en_GB.ts
+++ garuda-downloader_en_GB.ts
@@ -5,42 +5,50 @@
     <name>GarudaDownloader</name>
     <message>
         <location filename="garudadownloader.ui" line="32"/>
+        <location filename="build/garuda-downloader_autogen/include/ui_garudadownloader.h" line="169"/>
         <source>GarudaDownloader</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="garudadownloader.ui" line="55"/>
+        <location filename="build/garuda-downloader_autogen/include/ui_garudadownloader.h" line="170"/>
         <source>Select Seed File (optional)</source>
         <oldsource>Select Seed File</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="garudadownloader.ui" line="85"/>
+        <location filename="build/garuda-downloader_autogen/include/ui_garudadownloader.h" line="171"/>
         <source>Download</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="garudadownloader.ui" line="159"/>
+        <location filename="build/garuda-downloader_autogen/include/ui_garudadownloader.h" line="174"/>
         <source>Edition: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="garudadownloader.ui" line="167"/>
+        <location filename="build/garuda-downloader_autogen/include/ui_garudadownloader.h" line="175"/>
         <source>Dr460nized</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="garudadownloader.ui" line="172"/>
+        <location filename="build/garuda-downloader_autogen/include/ui_garudadownloader.h" line="176"/>
         <source>Barebones</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="garudadownloader.ui" line="115"/>
+        <location filename="build/garuda-downloader_autogen/include/ui_garudadownloader.h" line="172"/>
         <source>Launch Etcher or something idk</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="garudadownloader.ui" line="152"/>
+        <location filename="build/garuda-downloader_autogen/include/ui_garudadownloader.h" line="173"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Idle&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation type="unfinished"></translation>
     </message>
