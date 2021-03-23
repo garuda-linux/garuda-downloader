c407d6512ef7ee7c12fb96b9f16707170d24ed77
TotallyNotElite <totallynotelite@gmail.com>
2021-03-23 10:06:47 +0100

    Dark mode

--- main.cpp
+++ main.cpp
@@ -4,10 +4,24 @@
 #endif
 
 #include <QApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile f(":qdarkstyle/style.qss");
+
+    if (!f.exists()) {
+      qWarning() << "Unable to set stylesheet, file not found";
+    } else {
+      f.open(QFile::ReadOnly | QFile::Text);
+      QTextStream ts(&f);
+      qApp->setStyleSheet(ts.readAll());
+    }
+
     GarudaDownloader w;
     w.show();
 #if __linux__
